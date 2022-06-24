7b290a4ae7444439e7298645ecd88d3f5254b2ff
Crowdbotics <team@crowdbotics.com>
2022-06-24 12:55:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "adventure_coin_35885",
+  url: "https://adventure_coin_35885.botics.co",
+  api: "https://adventure_coin_35885.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
